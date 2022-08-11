747662f4dd26500461c4adf91bf9fae9a6b1402c
Sergey Ermolenko <sergey.ermolenko@telexy.com>
2022-08-11 08:40:31 -0700

    new probes

--- src/MbfcSchemaProbe/Program.cs
+++ src/MbfcSchemaProbe/Program.cs
@@ -8,9 +8,10 @@ var host = hostBuilder.Build();
 var env = host.Services.GetRequiredService<IHostEnvironment>();
 
 var fp = env.ContentRootFileProvider;
-var fileName = "Schemas/empty.schema.jsonc";
+//var fileName = "Schemas/empty.schema.jsonc";
 //var fileName = "Schemas/mbfc_sdk.schema.jsonc";
 //var fileName = "Schemas/draft-07.schema.jsonc";
+var fileName = "Schemas/empty_draft-07.schema.jsonc";
 //var fileName = "Schemas/draft-04.schema.jsonc";
 
 var fi = fp.GetFileInfo(fileName);
@@ -20,7 +21,7 @@ var srcJson = await reader.ReadToEndAsync();
 var schema = await JsonSchema.FromJsonAsync(fi.CreateReadStream());
 var ver = schema.SchemaVersion;
 var df = schema.Default;
-schema.SchemaVersion = "http://json-schema.org/draft-07/schema#";
+//schema.SchemaVersion = "http://json-schema.org/draft-07/schema#";
 var dstJson = schema.ToJson();
 var equal = string.Equals(srcJson, dstJson);
 Console.WriteLine("Equal: " + equal);
